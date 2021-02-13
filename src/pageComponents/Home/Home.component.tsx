3f989547bbd84b61e522d3b723eb320402d29293
seventrio <dimakrikunov12@gmail.com>
2021-02-13 21:03:24 +0200

    add layout

--- src/pageComponents/Home/Home.component.tsx
+++ src/pageComponents/Home/Home.component.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { PageTitle } from '@/pageComponents/Home/Home.styles';
+import MainLayout from '@/layouts/MainLayout/MainLayout.component';
 
-const HomePageComponent: React.FC = () => <PageTitle>Hello World</PageTitle>;
+const HomePageComponent: React.FC = () => (
+  <MainLayout title="Next.js + TypeScript Example">
+    <PageTitle>Hello World</PageTitle>
+  </MainLayout>
+);
 
 export default HomePageComponent;
